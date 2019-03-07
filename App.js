343bbd9d29e2acc41fad07c65def0532320a859f
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2019-03-07 15:48:56 +0100

    Password recovery ok - Product page development in progress

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import {
   createSwitchNavigator,
   createStackNavigator,
-  createAppContainer,
+  createAppContainer
 } from "react-navigation";
 
 import AuthLoadingScreen from "./containers/AuthLoadingScreen";
@@ -9,26 +9,27 @@ import SignInScreen from "./containers/SignInScreen";
 import SignUpScreen from "./containers/SignUpScreen";
 import HomeScreen from "./containers/HomeScreen";
 import TabNavigator from "./containers/TabNavigator";
-
-// import TabNavigator from "./src/containers/TabNavigator";
+import ProductScreen from "./containers/ProductScreen";
+import PasswordResetScreen from "./containers/PasswordResetScreen";
 
 const AppStack = createStackNavigator({
   Tab: TabNavigator,
   Home: HomeScreen,
   SignIn: SignInScreen,
   SignUp: SignUpScreen,
+  PasswordReset: PasswordResetScreen
 });
-const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+const AuthStack = createStackNavigator({ SignIn: ProductScreen });
 
 export default createAppContainer(
   createSwitchNavigator(
     {
       AuthLoading: AuthLoadingScreen,
       App: AppStack,
-      Auth: AuthStack,
+      Auth: AuthStack
     },
     {
-      initialRouteName: "AuthLoading",
+      initialRouteName: "AuthLoading"
     }
   )
 );
---
+++ containers/SellerProfileScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text } from "react-native";
+
+class SellerProfile extends React.Component {
+  render() {
+    return <Text>Profil vendeur</Text>;
+  }
+}
+
+export default SellerProfile;

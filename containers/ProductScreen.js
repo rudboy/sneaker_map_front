142920cd488a58d1d311e49e9cd226d195b5181a
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2019-03-08 18:19:08 +0100

    update product screen : click to call (phone icon)

--- containers/ProductScreen.js
+++ containers/ProductScreen.js
@@ -6,11 +6,13 @@ import {
   StyleSheet,
   Image,
   TouchableOpacity,
-  ActivityIndicator
+  ActivityIndicator,
+  AppRegistry
 } from "react-native";
 import { MapView } from "expo";
 import { Ionicons, FontAwesome } from "@expo/vector-icons";
 import axios from "axios";
+import Communications from "react-native-communications";
 
 class ProductScreen extends React.Component {
   state = {
@@ -19,23 +21,36 @@ class ProductScreen extends React.Component {
     title: "",
     description: "",
     price: "",
+    etat: "",
     size: "",
+    localisation: null,
     isFavorite: false,
     isOpen: false,
     favory: [],
-    userToken: null
+    userToken: null,
+    phone: null
+  };
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      headerStyle: {
+        height: 5
+      }
+    };
   };
 
   componentDidMount = async () => {
     const response = await axios.get(
       "http://localhost:5500/get_product_info?id=5c80f22710b1f405ffdfd7b5" // remplacer l'id par la props reçue de la page précédente
     );
-    // console.log("response.data ", response.data);
+    console.log("response.data ", response.data);
     const title = response.data.title;
     const description = response.data.description;
     const price = response.data.price;
+    const etat = response.data.etat;
     const size = response.data.size;
     const productId = response.data._id;
+    const localisation = response.data.localisation;
 
     // const userInfo = await AsyncStorage.getItem("userInfo");
     // console.log("userInfo ", userInfo);
@@ -55,9 +70,12 @@ class ProductScreen extends React.Component {
         title: title,
         description: description,
         price: price,
+        etat: etat,
         size: size,
         favory: userResponse.data.favory,
-        userToken: userResponse.data.token
+        userToken: userResponse.data.token,
+        phone: userResponse.data.phone,
+        localisation: localisation
       },
       () => {
         // vérifier si l'id produit se trouve dans le tableau de favoris
@@ -126,7 +144,7 @@ class ProductScreen extends React.Component {
     return (
       <Ionicons
         onPress={this.handleFavorite}
-        style={{ position: "absolute", right: 8, top: 5 }}
+        style={{ position: "absolute", right: 8, top: 12 }}
         name={this.state.isFavorite === false ? "ios-heart-empty" : "ios-heart"}
         size={35}
         color={this.state.isFavorite === true ? "red" : null}
@@ -138,7 +156,7 @@ class ProductScreen extends React.Component {
     // Modifier tableau de favoris de l'utilisateur:
     // appeler la route update user
     const response = await axios.post(
-      "http://localhost:5500/update_user_info",
+      "http://192.168.86.54:5500/update_user_info",
       {
         //body
         favory: this.state.productId
@@ -158,7 +176,7 @@ class ProductScreen extends React.Component {
 
   render() {
     if (this.state.isLoading === true) {
-      return <ActivityIndicator />;
+      return <ActivityIndicator style={{ flex: 1 }} />;
     } else {
       return (
         <>
@@ -174,25 +192,87 @@ class ProductScreen extends React.Component {
               />
               {this.renderFavorite()}
             </View>
-            <Text style={styles.title}>{this.state.title}</Text>
-            <Text style={styles.title}>Description</Text>
-            <TouchableOpacity onPress={this.handlePress}>
-              {this.renderDesc()}
-            </TouchableOpacity>
-            <Text style={styles.title}>Pointure</Text>
-            <Text style={styles.p}>{this.state.size}</Text>
-            <Text style={styles.title}>Prix</Text>
-            <Text style={styles.p}>{this.state.price} €</Text>
-            <Text style={styles.title}>Localisation</Text>
+            <View style={styles.contentWrapper}>
+              <Text style={styles.title}>{this.state.title}</Text>
+              <Text style={styles.title}>Description</Text>
+              <TouchableOpacity onPress={this.handlePress}>
+                {this.renderDesc()}
+              </TouchableOpacity>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  alignItems: "center"
+                }}
+              >
+                <View
+                  style={{
+                    alignItems: "center",
+                    flex: 1
+                  }}
+                >
+                  <Text style={styles.title}>Pointure</Text>
+
+                  <Text style={styles.p}>{this.state.size}</Text>
+                </View>
+                <View
+                  style={{
+                    alignItems: "center",
+                    flex: 1
+                  }}
+                >
+                  <Text style={styles.title}>Prix</Text>
+
+                  <Text style={styles.p}>{this.state.price} €</Text>
+                </View>
+                <View
+                  style={{
+                    alignItems: "center",
+                    flex: 1
+                  }}
+                >
+                  <Text style={styles.title}>Etat</Text>
+
+                  <Text style={styles.p}>
+                    {this.state.etat === true ? "Neuf" : "Occasion"}
+                  </Text>
+                </View>
+              </View>
+              <Text style={styles.title}>Localisation</Text>
+            </View>
+
             <MapView
-              style={{ height: 180 }}
+              style={{ height: 250 }}
               initialRegion={{
-                latitude: 48.856614,
-                longitude: 2.3522219,
-                latitudeDelta: 0.09,
-                longitudeDelta: 0.04
+                latitude: this.state.localisation[1],
+                longitude: this.state.localisation[0],
+                latitudeDelta: 0.025,
+                longitudeDelta: 0.0029
               }}
-            />
+            >
+              <MapView.Marker
+                coordinate={{
+                  latitude: this.state.localisation[1],
+                  longitude: this.state.localisation[0]
+                }}
+                title={"La sneaker dont tu rêves"}
+                description={"Elle est là, elle t'attend !"}
+              >
+                <Image
+                  source={{
+                    uri:
+                      "https://www.lesitedelasneaker.com/wp-content/images/2018/04/air-jordan-3-katrina-136064-116-3.jpg"
+                  }}
+                  style={{
+                    width: 60,
+                    height: 60,
+                    borderRadius: 30,
+                    borderWidth: 1,
+                    borderColor: "grey"
+                  }}
+                />
+              </MapView.Marker>
+            </MapView>
           </ScrollView>
           <View
             style={{
@@ -205,18 +285,30 @@ class ProductScreen extends React.Component {
               onPress={() => this.props.navigation.navigate("Chat")}
               style={styles.iconWrapper}
             >
-              <Ionicons name="ios-chatboxes" size={50} color="black" />
+              <Ionicons name="ios-chatboxes" size={30} color="black" />
               <Text>Message</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={styles.iconWrapper}>
-              <FontAwesome name="phone" size={50} color="black" />
-              <Text>Contacter</Text>
+            <TouchableOpacity
+              style={styles.iconWrapper}
+              disabled={this.state.phone === null ? true : false}
+              onPress={() => Communications.phonecall(this.state.phone, true)}
+            >
+              <FontAwesome
+                name="phone"
+                size={30}
+                color={this.state.phone === null ? "grey" : "black"}
+              />
+              <Text
+                style={this.state.phone === null ? styles.grey : styles.black}
+              >
+                Contacter
+              </Text>
             </TouchableOpacity>
             <TouchableOpacity
               onPress={() => this.props.navigation.navigate("SellerProfile")}
               style={styles.iconWrapper}
             >
-              <Ionicons name="ios-person" size={50} color="black" />
+              <Ionicons name="ios-person" size={30} color="black" />
               <Text>Profil vendeur</Text>
             </TouchableOpacity>
           </View>
@@ -232,11 +324,17 @@ const styles = StyleSheet.create({
   container: {
     // flex: 1,
     // justifyContent: "center",
-    padding: 16
+    // paddingHorizontal: 16,
+    // paddingBottom: 50,
+    marginTop: 0
+  },
+  contentWrapper: {
+    paddingHorizontal: 16
   },
   productPic: {
-    width: "100%",
-    height: 180
+    // width: "100%",
+    height: 425,
+    resizeMode: "cover"
   },
   title: {
     fontSize: 24,
@@ -251,13 +349,20 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     paddingHorizontal: 16,
-    marginVertical: 16,
-    height: 80
+    marginTop: 6,
+    marginBottom: 10,
+    height: 56
   },
   iconWrapper: {
     flex: 1,
     height: "100%",
     alignItems: "center",
     justifyContent: "space-between"
+  },
+  grey: {
+    color: "grey"
+  },
+  black: {
+    color: "black"
   }
 });

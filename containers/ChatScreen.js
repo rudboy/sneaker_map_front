1652f3af63b587815300f5ca5a238a36ff1c2b91
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2019-03-12 10:07:32 +0100

    chatscreen in progress...

--- containers/ChatScreen.js
+++ containers/ChatScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
-import { View, Platform, KeyboardAvoidingView } from "react-native";
+import { Platform, KeyboardAvoidingView } from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
+import SocketIOClient from "socket.io-client";
+// import { AsyncStorage } from "react-native";
+import axios from "axios";
 
 const user = {
   _id: String(Math.random()), // récupérer l'id via async storage
@@ -8,29 +11,29 @@ const user = {
 };
 
 class Chat extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Creating the socket-client instance will automatically connect to the server.
+    this.socket = SocketIOClient("http://localhost:3000");
+  }
   state = {
-    messages: []
+    messages: [],
+    id: null
   };
 
-  componentDidMount() {
-    this.ws = new WebSocket("ws://lereacteur-ws-server.herokuapp.com/");
-    this.ws.addEventListener("message", event => {
-      const message = JSON.parse(event.data);
-      this.setState({
-        messages: [message, ...this.state.messages]
-      });
-    });
-  }
+  componentDidMount = async () => {
+    const response = await axios.get("http://localhost:3000/get_messages");
+    console.log(response.data);
+  };
 
-  componentWillUnmount() {
-    this.ws.close();
-  }
+  componentWillUnmount() {}
 
   handleSend = (messages = []) => {
-    const message = messages[0];
-    this.ws.send(JSON.stringify(message));
-    this.setState({
-      messages: [message, ...this.state.messages]
+    this.socket.on("message", message => {
+      const oldMessages = this.state.messages;
+      // React will automatically rerender the component when a new message is added.
+      this.setState({ messages: oldMessages.concat(message) });
     });
   };
 

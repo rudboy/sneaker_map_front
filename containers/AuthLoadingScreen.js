275b3d825ec1f1d1e7b6233d291afd7899b83131
Kevin N <45763760+Kevinnko@users.noreply.github.com>
2019-03-20 15:45:45 +0100

    modified authloading

--- containers/AuthLoadingScreen.js
+++ containers/AuthLoadingScreen.js
@@ -4,13 +4,15 @@ import Axios from "axios";
 
 class AuthLoadingScreen extends React.Component {
   componentDidMount = async () => {
-    const userInfo = await AsyncStorage.getItem("userInfo");
-
+    let userInfo = await AsyncStorage.getItem("userInfo");
+    userInfo = JSON.parse(userInfo);
     if (userInfo) {
-      const response = Axios.get(
+      const response = await Axios.get(
         "https://sneaker-map-api.herokuapp.com/get_my_user_info?token=" +
           userInfo.token
       );
+      console.log("response ", response.data);
+
       if (response.data === undefined) {
         this.props.navigation.navigate("Auth");
       } else {
